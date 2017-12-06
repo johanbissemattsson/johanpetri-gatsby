cf885bd415f4dab37962c917181fc17c21f03834
Johan Bisse Mattsson <johan@johanbissemattsson.se>
2017-12-06 16:10:52 +0100

    Buttons

--- src/templates/page.js
+++ src/templates/page.js
@@ -44,9 +44,10 @@ class Page extends Component {
       centerPadding: 0,
       variableWidth: true,
       lazyload: true,
-      arrows: false,
-      responsive: [ { breakpoint: 768, settings: { slidesToShow: 3} }, { breakpoint: 1024, settings: { slidesToShow: 2 } }]     
-      
+      arrows: true,
+      responsive: [ { breakpoint: 768, settings: { slidesToShow: 3} }, { breakpoint: 1024, settings: { slidesToShow: 2 } }],  
+      nextArrow: (<button {...this.props}><i class="material-icons" style={{fontStyle: "normal"}}>chevron_right</i></button>),
+      prevArrow: (<button {...this.props}><i class="material-icons" style={{fontStyle: "normal"}}>chevron_left</i></button>),
     };
 
     return (
