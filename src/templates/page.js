385b76e9c183699f0868a92e4c65691cf42be7af
Johan Bisse Mattsson <johan@johanbissemattsson.se>
2017-12-06 16:20:13 +0100

    No arrows

--- src/templates/page.js
+++ src/templates/page.js
@@ -44,10 +44,8 @@ class Page extends Component {
       centerPadding: 0,
       variableWidth: true,
       lazyload: true,
-      arrows: true,
+      arrows: false,
       responsive: [ { breakpoint: 768, settings: { slidesToShow: 3} }, { breakpoint: 1024, settings: { slidesToShow: 2 } }],  
-      //nextArrow: (<button {...this.props}><i class="material-icons" style={{fontStyle: "normal"}}>chevron_right</i></button>),
-      //prevArrow: (<button {...this.props}><i class="material-icons" style={{fontStyle: "normal"}}>chevron_left</i></button>),
     };
 
     return (
